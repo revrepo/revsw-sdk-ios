b8bd091648f224b44da131b3a035d27ab63f7ce1
Alexander Mamchich <alexander@tundramobile.com>
2016-02-04 19:59:36 +0200

    Full-functional repack

--- RevSDK/RevSDK/libquic_repack/boringssl/fuzz/cert.cc
+++
@@ -1,7 +0,0 @@
-#include <openssl/x509.h>
-
-extern "C" int LLVMFuzzerTestOneInput(uint8_t *buf, size_t len) {
-  const uint8_t *bufp = buf;
-  X509_free(d2i_X509(NULL, &bufp, len));
-  return 0;
-}
