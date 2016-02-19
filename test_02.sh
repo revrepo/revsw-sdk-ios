71940562137942e744a7c3e9b399674d3f84e4e7
Alexander Mamchich <alexander@tundramobile.com>
2016-02-19 22:24:45 +0200

    Test scripts added

---
+++ test_02.sh
@@ -0,0 +1,12 @@
+SDK_PATH=./DerivedData/Build/Products/Release-iphoneos
+
+rm -rf ./DerivedData
+mkdir -p ${SDK_PATH}
+
+cp -R ./RevSDK/export/RevSDK.framework ${SDK_PATH}/RevSDK.framework
+
+xcodebuild test \
+-workspace RevSDK.xcworkspace \
+-scheme RevTest\ App \
+-destination 'platform=iOS Simulator,OS=9.2,name=iPhone 6' \
+
